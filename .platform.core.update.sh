d8b8d1268aac5f937348d2d3e5f9243b9c01f390
Vincenzo Russo <v@artetecha.com>
2021-09-20 17:40:58 +0200

    Use .version instead of .environment

--- .platform.core.update.sh
+++ .platform.core.update.sh
@@ -4,6 +4,6 @@ CURRENT_VERSION=$(jq -r ".version" package.json)
 RELEASE_URL=https://api.github.com/repos/Requarks/wiki/releases/latest
 WIKIJS_VERSION=$(curl -s $RELEASE_URL| jq -r '.tag_name')
 if [ "$WIKIJS_VERSION" != "$CURRENT_VERSION" ]; then
-  echo "export WIKIJS_VERSION='$WIKIJS_VERSION'" > .environment
+  echo "export WIKIJS_VERSION='$WIKIJS_VERSION'" > .version
   git commit -m "Updated Wiki.js ($CURRENT_VERSION to $WIKIJS_VERSION)."
 fi

5db89320b253bebdb1528c0f89b129556e4b6020
Vincenzo Russo <v@artetecha.com>
2021-09-20 15:01:44 +0200

    Improve download&extract; fix sed

--- .platform.build.sh
+++ .platform.build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/Requarks/wiki/releases/download/2.5.201/wiki-js.tar.gz
-tar xzf wiki-js.tar.gz -C .
+wget --quiet -c https://github.com/Requarks/wiki/releases/download/2.5.201/wiki-js.tar.gz -O - | tar -xz
 
 DB_HOST=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r '.database[0].host')
 DB_PORT=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r '.database[0].port')
@@ -9,10 +8,12 @@ DB_USERNAME=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r '.database[
 DB_PASSWORD=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r '.database[0].password')
 DB_DATABASE=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r '.database[0].path')
 
-sed "s|port: 3000|port: $PORT|g" config.sample.yml > config.yml
-sed -i "s|host: localhost|host: $DB_HOST|g" config.yml
-sed -i "s|port: 5432|port: $DB_PORT|g" config.yml
-sed -i "s|pass: wikijsrocks|pass: $DB_PASSWORD|g" config.yml
-sed -i "s|user: wikijs|user: $DB_USERNAME|g" config.yml
-sed -i "s|db: wiki|db: $DB_DATABASE|g" config.yml
-sed -i "s|bindIP: 0.0.0.0|bindIP: 127.0.0.1|g" config.yml
+sed "
+  s|port: 3000|port: $PORT|g
+  s|host: localhost|host: $DB_HOST|g
+  s|port: 5432|port: $DB_PORT|g
+  s|pass: wikijsrocks|pass: $DB_PASSWORD|g
+  s|user: wikijs|user: $DB_USERNAME|g
+  s|db: wiki|db: $DB_DATABASE|g
+  s|bindIP: 0.0.0.0|bindIP: 127.0.0.1|g
+  " config.sample.yml > config.yml

c4a831511464b50f592e251bc1c3e05bdb9ef858
Vincenzo Russo <v@artetecha.com>
2021-09-20 17:50:26 +0200

    Fix source operations

--- .platform.core.update.sh
+++ .platform.core.update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 RELEASE_URL=https://api.github.com/repos/Requarks/wiki/releases/latest
-CURRENT_VERSION=$(jq -r ".version" package.json)
+CURRENT_VERSION=$(cat .version)
 LATEST_VERSION=$(curl -s $RELEASE_URL| jq -r '.tag_name')
 if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
   echo $LATEST_VERSION > .version
+  git add .version
   git commit -m "Updated Wiki.js ($CURRENT_VERSION to $LATEST_VERSION)."
 fi

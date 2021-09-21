2c057f1c825a38ad6db64c2e1aa76732c1cf011a
Vincenzo Russo <v@artetecha.com>
2021-09-21 15:12:24 +0200

    Update core update script

--- .platform.core.update.sh
+++ .platform.core.update.sh
@@ -7,4 +7,6 @@ if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
   echo $LATEST_VERSION > .version
   git add .version
   git commit -m "Updated Wiki.js ($CURRENT_VERSION to $LATEST_VERSION)."
+else
+  echo "No core updates found. Wiki.js remains on version $CURRENT_VERSION"
 fi

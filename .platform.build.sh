b4fbd78ed5b4db83aaff1d1079bf75741e25e625
Vincenzo Russo <v@artetecha.com>
2021-09-22 10:34:33 +0200

    Fix curl command

--- .platform.build.sh
+++ .platform.build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -sS https://platform.sh/cli/installer | php
-curl -sS https://github.com/Requarks/wiki/releases/download/$(cat .version)/wiki-js.tar.gz -O - | tar -xz
+curl -sSL https://github.com/Requarks/wiki/releases/download/$(cat .version)/wiki-js.tar.gz | tar -xz
 ln -s config/config.yml config.yml
